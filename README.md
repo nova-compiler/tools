b4ba9b09a27ebac480c07e76ed69172636770bcb
Luis Garcia <lgarcia14@gmail.com>
2013-07-14 10:05:07 -0430

    Add strict mode to substitute.py
    In strict mode the script aborts if the text can't be successfully
    substituted.

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ The cake is a lie.
 
 For usage:
 <pre>
-python substitute.py -i &lt;input-file&gt; -o &lt;output-file&gt; [-c &lt;context-file&gt;]
+python substitute.py -i &lt;input-file&gt; -o &lt;output-file&gt; [-c &lt;context-file&gt;] [-s]
 </pre>
 
-Where &lt;input-file&gt;, &lt;output-file&gt;, and &lt;context-file&gt; are the paths of the respective files.
+Where &lt;input-file&gt;, &lt;output-file&gt;, and &lt;context-file&gt; are the paths of the respective files. The -s argument indicates to use strict mode, in this mode all warnings are treated as errors, and if any occurs the program aborts.
--- substitute.py
+++ substitute.py
@@ -2,7 +2,15 @@ import getopt, os, sys, json, io
 
 version = '0.1.0'
 
-def substitute_text(contents, context):
+class CommandArgumentsError(RuntimeError):
+	def __init__(self, arg):
+		self.args = arg
+
+class SubstituteError(RuntimeError):
+	def __init__(self, arg):
+		self.args = arg
+
+def substitute_text(contents, context, strict):
 	with io.BytesIO() as output:
 		index = 0
 		while index < len(contents):
@@ -12,7 +20,10 @@ def substitute_text(contents, context):
 					end = end + 1
 
 				if end == len(contents):
-					print('Warning: unmatched % was found. Ignored.')
+					if not strict:
+						print('Warning: unmatched % was found. Ignored.')
+					else:
+						raise SubstituteError(('unmatched % was found.',))
 				elif end - index == 1:
 					output.write('%%')
 				else:
@@ -20,7 +31,11 @@ def substitute_text(contents, context):
 					if variable_name in context:
 						output.write(context[variable_name].encode('utf-8'))
 					else:
-						print('Warning: variable %' + variable_name + '% was not found in context. Ignored.')
+						if not strict:
+							print('Warning: variable %' + variable_name + '% was not found in context. Ignored.')
+						else:
+							raise SubstituteError(('variable %' + variable_name + '% was not found in context.',))
+
 					
 				index = end + 1
 			else:
@@ -33,30 +48,34 @@ def read_context_file(context_file):
 		data = json.loads(file.read())
 		return data['context']
 
-def substitute_file(input_file_path, output_file_path, context_file):
-	with open(input_file_path, 'r') as input_file:
-		contents = input_file.read()
+def substitute_file(input_file_path, output_file_path, context_file, strict):
+	try:
+		with open(input_file_path, 'r') as input_file:
+			contents = input_file.read()
 
-		context = {}
-		if context_file != '':
-			context = read_context_file(context_file)
+			context = {}
+			if context_file != '':
+				context = read_context_file(context_file)
 
-		processed_output = substitute_text(contents, context)
+			processed_output = substitute_text(contents, context, strict)
 
-		with open(output_file_path, 'w') as output_file:
-			output_file.write(processed_output)
+			with open(output_file_path, 'w') as output_file:
+				output_file.write(processed_output)
 
-class CommandArgumentsError(RuntimeError):
-   def __init__(self, arg):
-      self.args = arg
+		return 0
+	except SubstituteError as err:
+		print('Error: ' + err.args[0])
+		print('Operation aborted.')
+		return 1
 
 def read_arguments(args):
 	input_file = ''
 	output_file = ''
 	context_file = ''
+	strict = False
 
 	try:
-		opts, args = getopt.getopt(args, 'i:o:c:', [ 'input=', 'output=', 'context=' ])
+		opts, args = getopt.getopt(args, 'i:o:c:s', [ 'input=', 'output=', 'context=', 'strict' ])
 		for opt, arg in opts:
 			if opt in ('-i', '--input'):
 				input_file = arg
@@ -64,13 +83,15 @@ def read_arguments(args):
 				output_file = arg
 			elif opt in ('-c', '--context'):
 				context_file = arg
+			elif opt in ('-s', '--strict'):
+				strict = True
 
 		if input_file == '' or output_file == '':
 			raise CommandArgumentsError(('you must specify both input and output file. See usage.',))
 	except getopt.GetoptError as err:
 		raise CommandArgumentsError(err.args)
 
-	return input_file, output_file, context_file
+	return input_file, output_file, context_file, strict
 
 def usage():
 	print('Usage: python substitute.py -i <input-file> -o <output-file> [-c <context-file>]')
@@ -81,13 +102,14 @@ def usage():
 
 def main(args):
 	try:
-		input_file, output_file, context_file = read_arguments(args)
+		input_file, output_file, context_file, strict = read_arguments(args)
 	except CommandArgumentsError as err:
 		print('Error: ' + err.args[0])
 		print('')
 		usage()
 		sys.exit(2)
 
-	substitute_file(input_file, output_file, context_file)
+	return_code = substitute_file(input_file, output_file, context_file, strict)
+	sys.exit(return_code)	
 
 main(sys.argv[1:])
